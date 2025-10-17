921c9a2b74207ff4345897139a705cb8dd83466f
Jarvis <mengda636431@gmail.com>
2025-10-17 21:01:55 +0800

    [fea] 配置 eslint 和 prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': 'error',
+  },
+};
